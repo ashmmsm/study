972ae51cce6563a94dfb1d24b3695a45581db799
Sumaya <ashirmamatovasumaya@gmail.com>
2024-10-25 22:17:29 +0600

    Create practice_site.py

---
+++ practice_site.py
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./style.css">
+</head>
+<body>
+    <!-- <div class="box">
+        <div>
+        
+            <img class="photo-1" src="https://proprikol.ru/wp-content/uploads/2020/11/kartinki-piczcza-40.jpeg" alt="">
+        </div>
+        <div>
+            <img class="photo-2" src="https://cdn-zaryakubani.storage.yandexcloud.net/uploads/2023/07/front_en.3.full_-scaled.jpg" alt="">
+        </div>
+        <div>
+            <div>
+                
+            </div>
+            <img class="photo-3" src="https://www.fonstola.ru/images/201707/fonstola.ru_267133.jpg" alt="">
+        </div>
+        <div>
+            <img class="photo-4" src="https://podacha-blud.com/uploads/posts/2022-12/1670981165_1-podacha-blud-com-p-venskie-vafli-s-morozhenim-foto-1.jpg" alt="">
+        </div>
+        <div>
+            <img class="photo-5" src="https://mykaleidoscope.ru/x/uploads/posts/2022-10/1666444988_3-mykaleidoscope-ru-p-vidi-keksov-instagram-3.jpg" alt="">
+        </div>
+        <div>
+            <img class="photo-6" src="https://podacha-blud.com/uploads/posts/2022-10/1665874073_1-podacha-blud-com-p-gaz-napitki-krasivie-foto-1.jpg" alt="">
+        </div>
+    </div
+
+   > -->
+
+
+   <header>
+    <img class="photo" src="https://klike.net/uploads/posts/2019-11/1574605343_39.jpg" alt="">
+    <input  type="text">
+    <img class="poisk" src="https://www.pngmart.com/files/8/Search-Button-PNG-Free-Image.png" alt="">
+    <ul>
+        <li class="text-1">contact</li>
+        <li class="text-2">tel</li>
+        <li class="text-3">email</li>
+        <li class="text-4">adress</li>
+    </ul>
+    </header>
+    <section class="box-1">
+<span>Lorem ipsum dolor sit amet consectetur adipisicing elit. At libero, adipisci magnam veritatis aperiam earum sunt eum perferendis, recusandae veniam eius nam amet tempore perspiciatis. Iure id, temporibus sed earum, consectetur harum, similique perspiciatis cupiditate laudantium corporis neque corrupti nam veniam ullam vero voluptatibus. Vitae similique adipisci ab, voluptatum quia harum dolore ipsam quibusdam doloribus enim, itaque quis facilis dolorem! Maiores nihil non obcaecati mollitia beatae vitae excepturi nam sapiente voluptates suscipit ducimus rem ab quod, nostrum laboriosam esse ipsum. Expedita aperiam quisquam obcaecati debitis cumque? Veniam fuga soluta molestiae, at voluptatem quasi asperiores laudantium aliquid voluptates voluptatibus saepe a?</span>
+    </section>
+    <section class="box-2">
+<span>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Placeat quo at quae, quos ab dicta laudantium cum officiis unde nulla? Sint consequuntur sit eaque laborum corporis illum voluptatibus consequatur id qui aliquam beatae officia dolorem facere placeat dolor, cum mollitia dicta voluptate incidunt impedit maxime molestias expedita vitae. Est repellendus saepe, vel cupiditate officiis ea fugit nihil. Harum delectus quisquam maiores iure illo perspiciatis doloribus tempore non distinctio esse possimus vero quibusdam ipsam aliquam dolor eum aut deserunt recusandae perferendis autem aperiam, nemo illum a! Doloremque, autem aut. Nam non aliquam itaque consectetur perspiciatis ducimus necessitatibus recusandae beatae vel, voluptates tenetur maxime aut obcaecati possimus ratione aliquid inventore est. Velit veritatis est neque omnis saepe cumque doloribus adipisci quidem rerum beatae. Quos, sint culpa! Hic ullam consectetur ex ut deleniti eveniet vitae corporis iusto vero temporibus illum animi ipsam fuga quasi, voluptatum dicta consequuntur. Est necessitatibus perspiciatis autem eligendi amet exercitationem aspernatur mollitia accusamus sequi, laborum culpa. Officiis perspiciatis dolor voluptate tempore, quia, sapiente iure vitae quas omnis repellat sed, id similique praesentium ratione. Est quaerat vitae possimus commodi iusto tempora! Eum distinctio cum ut consectetur, blanditiis laboriosam pariatur unde assumenda officia tenetur fugit aut neque inventore in, doloribus debitis?
+</span>
+    </section>
+   </header>
+
+</body>
+</html>
+
+
+
+
+
+
+/* .box{
+    border: 5px solid red;
+    height: 900px;
+    display: flex;
+    justify-content: space-between;
+}
+.photo-1{
+    width: 200px;
+    height: 300px;
+}
+.photo-2{
+    width: 200px;
+    height: 300px;
+}
+.photo-3{
+    width: 200px;
+    height: 300px;
+}
+.photo-4{
+    width: 200px;
+    height: 300px;
+}
+.photo-5{
+    width: 200px;
+    height: 300px;
+}
+.photo-6{
+    width: 200px;
+    height: 300px;
+} */
+
+header{
+    height: 70px;
+    border: 2px solid red;
+    background-color: rgb(98, 77, 25);
+    display: flex;
+    align-items: center;
+}
+body{
+    background-image: url(https://img2.akspic.ru/attachments/crops/0/4/8/9/99840/99840-rastitelnost-vodoem-prirodnyj_zapovednik-vodopad-gidroresursy-2560x1440.jpg)
+}
+.photo{
+    height: 70px;
+    width: 70px;
+    border-radius: 50%;
+}
+input{
+    height: 20px;
+    margin-left: 30px;
+}
+ul{
+    display: flex;
+    justify-content: space-between;
+    width: 600px;
+    list-style: none;
+}
+.poisk{
+    height: 30px;
+    margin-left: 10px;
+}
+.text-1:hover{
+    color: azure;
+}
+.text-2:hover{
+    color: aliceblue;
+}
+.text-3:hover{
+    color: beige;
+}
+.text-4:hover{
+    color: rgb(229, 18, 60);
+}
+.box-1{
+    margin-top: 10px;
+    background-color: rgb(123, 100, 72);
+}
+.box-2{
+    margin-top: 10px;
+    background-color: rgb(82, 56, 107);
+}
